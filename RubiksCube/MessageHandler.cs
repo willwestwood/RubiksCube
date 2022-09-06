e9c7f3e47b3d8656d153edfcfdcde74114bd4afb
William Westwood <williamwestwood@williams-mbp.lan>
2022-09-06 12:07:49 +0100

    Add missing changes. Add MessageHandler.

---
+++ RubiksCube/MessageHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace RubiksCube
+{
+    static public class MessageHandler
+    {
+        static public String GetCubeState(Cube cube)
+        {
+            return JsonSerializer.Serialize(cube);
+        }
+
+        static public void InterpretCommand(String message)
+        {
+            try
+            {
+                var doc = JsonSerializer.Deserialize<JsonDocument>(message);
+
+                Console.WriteLine(doc.RootElement.GetString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
--- RubiksCube/WebSocketService.cs
+++ RubiksCube/WebSocketService.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using WebSocketSharp;
 using WebSocketSharp.Server;
+using System.Net;
 
 namespace RubiksCube
 {
     public class Echo : WebSocketBehavior
     {
+        public Echo()
+        {
+            _cube = new Cube(3);
+        }
+
+        protected override void OnOpen()
+        {
+            Send("Welcome to Rubiks Cube!");
+            Send(MessageHandler.GetCubeState(_cube));
+        }
+
         protected override void OnMessage(MessageEventArgs e)
         {
             var msg = e.Data;
@@ -13,15 +25,20 @@ namespace RubiksCube
             Console.WriteLine(e.Data);
 
             Send(msg + " returned");
+            MessageHandler.InterpretCommand(msg);
         }
+
+        private Cube _cube;
     }
 
     public class WebSocketService
     {
         public WebSocketService()
         {
-            _server = new WebSocketServer("ws://127.0.0.1:3000");
-            _server.AddWebSocketService<Echo>("/echo");           
+            IPHostEntry ipEntry = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress[] addr = ipEntry.AddressList;
+            _server = new WebSocketServer("ws://" + addr[0] + ":3000");
+            _server.AddWebSocketService<Echo>("/echo");
         }
 
         public void Start()
